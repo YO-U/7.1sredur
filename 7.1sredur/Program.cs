9e91bb712ea865214b975fbdb67e21ea54807ed1
enoxy <enoxy@DESKTOP-8BHQKRR>
2021-12-16 10:37:12 +0200

    Добавьте файлы проекта.

---
+++ 7.1sredur/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+namespace _7._2sredur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+            string res = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((short)str[i] >= 65 && (short)str[i] <= 122) continue;
+                res += str[i];
+            }
+            Console.WriteLine(res);
+
+
+
+        }
+    }
+}
